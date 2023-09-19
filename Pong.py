cb79f700d7e25028b8c17af8c8a58f53fc41e280
gustavo-dotcom <gustavo.oliveira2003@hotmail.com>
2023-09-19 10:54:23 -0300

    Desenhando bola na janela e inicializando as variáveis

--- Pong.py
+++ Pong.py
@@ -2,10 +2,18 @@ import pygame
 
 pygame.init()
 
-WIDTH, HEIGHT = 1000, 600                               #Tamanho da tela definido para 1000 pixels por 600 pixels
-window = pygame.display.set_mode((WIDTH, HEIGHT))       #Criando uma janela vazia do pygame
+WIDTH, HEIGHT = 1000, 600                                       #Tamanho da tela definido para 1000 pixels por 600 pixels
+window = pygame.display.set_mode((WIDTH, HEIGHT))               #Criando uma janela vazia do pygame
+pygame.display.set_caption("Pong")                              #Atribuindo o nome da janela para Pong
+
+BLUE = (0, 0, 255)                                              #Variáveis da cor azul
+
+radius = 15                                                     #Raio da bola
+ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - 2                 #Posição x e y da bola
+
 run = True
-while run:                                              #Loop principal do jogo onde vai acontecer todas as animações e eventos
-    for i in pygame.event.get():                        #Armazena os eventos coletados pelo event.get() na variável i 
-        if i.type == pygame.QUIT:                       #Verifica se o usuário precionou o botão de sair
+while run:                                                      #Loop principal do jogo onde vai acontecer todas as animações e eventos
+    for i in pygame.event.get():                                #Armazena os eventos coletados pelo event.get() na variável i 
+        if i.type == pygame.QUIT:                               #Verifica se o usuário precionou o botão de sair
             run = False
+    pygame.draw.circle(window, BLUE, ball_x, ball_y, radius)    #Desenha a bola na janela
