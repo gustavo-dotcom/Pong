91cb1ba8e7d85331f227d4ba2503581b3e56dc85
gustavo-dotcom <gustavo.oliveira2003@hotmail.com>
2023-09-19 13:44:22 -0300

    adicionando os valores das variáveis das raquetes e criado a variável da cor vermelha

--- Pong.py
+++ Pong.py
@@ -7,9 +7,14 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))                               
 pygame.display.set_caption("Pong")                                                                  #Atribuindo o nome da janela para Pong
 
 BLUE = (0, 0, 255)                                                                                  #Variáveis da cor azul
+RED = (255, 0, 0)                                                                                   #Variáveis da cor vermelha
 
 radius = 15                                                                                         #Raio da bola
 ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius                                                #Posição x e y da bola
+
+paddle_width, paddle_height = 20, 120                                                               #Largura e altura das raquetes
+left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2                                         #Posição y das raquetes esquerda e direita
+left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH - (100 - paddle_width/2)                #Posição x das raquetes esquerda e direita
 
 run = True
 while run:                                                                                          #Loop principal do jogo onde vai acontecer todas as animações e eventos
