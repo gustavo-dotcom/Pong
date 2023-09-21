1f6c5420d07f815c560b8b764651c28ae2e10437
gustavo-dotcom <gustavo.oliveira2003@hotmail.com>
2023-09-21 16:48:49 -0300

    Correção de bug que não permitia que duas raquetes se movessem ao mesmo tempo

--- Pong.py
+++ Pong.py
@@ -35,9 +35,16 @@ while run:                                                                      
             if i.key == pygame.K_s:
                 left_paddle_vel = 0.9
         
-        if i.type == pygame.KEYUP:
-            right_paddle_vel = 0
-            left_paddle_vel = 0                                    
+        if i.type == pygame.KEYUP:                                                              #Movimento das raquetes direita e esquerda pelo uso das setas cima/baixo e w/s
+            if i.key == pygame.K_UP:
+                right_paddle_vel = 0
+            if i.key == pygame.K_DOWN:
+                right_paddle_vel = 0
+            if i.key == pygame.K_w:
+                left_paddle_vel = 0
+            if i.key == pygame.K_s:
+                left_paddle_vel = 0
+                                            
 
     if ball_y <= 0 + radius or ball_y >= HEIGHT - radius:                                           #Verificando se a bola encosta nas bordas de cima ou baixo e se sim inverte a direção
         ball_vel_y *= -1
@@ -49,11 +56,6 @@ while run:                                                                      
         ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
         ball_vel_x, ball_vel_y = 0.7, 0.7
 
-    ball_x += ball_vel_x                                                                            #Movimento da bola em x
-    ball_y += ball_vel_y                                                                            #Movimento da bola em y
-    left_paddle_y += left_paddle_vel                                                                #Movimento da raquete esquerda em y
-    right_paddle_y += right_paddle_vel                                                              #Movimento da raquete direita em y
-
     if left_paddle_y >= HEIGHT - paddle_height:                                                     #Verifica se as raquetes estão saindo da tela e se sim volta elas para dentro da janela
         left_paddle_y = HEIGHT - paddle_height
     if left_paddle_y <= 0:
@@ -62,7 +64,14 @@ while run:                                                                      
         right_paddle_y = HEIGHT - paddle_height
     if right_paddle_y <= 0:
         right_paddle_y = 0
-     
+
+    
+
+    ball_x += ball_vel_x                                                                            #Movimento da bola em x
+    ball_y += ball_vel_y                                                                            #Movimento da bola em y
+    left_paddle_y += left_paddle_vel                                                                #Movimento da raquete esquerda em y
+    right_paddle_y += right_paddle_vel                                                              #Movimento da raquete direita em y
+    
 
     pygame.draw.circle(window, BLUE, (ball_x, ball_y), radius)                                      #Desenha a bola na janela
     pygame.draw.rect(window, RED, (left_paddle_x, left_paddle_y, paddle_width, paddle_height))      #Variáveis da raquete esquerda
